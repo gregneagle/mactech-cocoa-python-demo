6a59654beb7bd0c359dd62df366c4f8f594f3741
Greg Neagle <gregneagle@mac.com>
2013-01-19 20:58:36 -0800

    Created and connected IBOutlets in MTMainWindowController.py; awakeFromNib method demonstrates connections.

--- Demo/MTMainWindowController.py
+++ Demo/MTMainWindowController.py
@@ -11,4 +11,14 @@ from Foundation import *
 from AppKit import *
 
 class MTMainWindowController(NSWindowController):
-    
+
+    applicationFld = IBOutlet()
+    installLocationFld = IBOutlet()
+    identifierFld = IBOutlet()
+    versionFld = IBOutlet()
+
+    def awakeFromNib(self):
+        self.applicationFld.setStringValue_(u'/path/to/Some.app')
+        self.installLocationFld.setStringValue_(u'/Applications')
+        self.identifierFld.setStringValue_(u'com.mactech.demo')
+        self.versionFld.setStringValue_(u'1.0')

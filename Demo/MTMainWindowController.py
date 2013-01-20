7d762d5dd10c2a8b1d8a4ebaf501b23883ac1f0c
Greg Neagle <gregneagle@mac.com>
2013-01-20 11:34:21 -0800

    Add getApplication_ method and display Open panel to select an application

--- Demo/MTMainWindowController.py
+++ Demo/MTMainWindowController.py
@@ -19,6 +19,37 @@ class MTMainWindowController(NSWindowController):
     identifierFld = IBOutlet()
     versionFld = IBOutlet()
     
+    def getApplicationPath(self):
+        '''Display NSOpenPanel to get the path to an
+        application to package.
+        Returns a pathname or None'''
+    
+        # instantiate open panel object
+        panel = NSOpenPanel.openPanel()
+        # allow only .app files
+        panel.setAllowedFileTypes_(['app'])
+        # set the intiallly displayed directory
+        # to /Applications
+        panel.setDirectoryURL_(
+            NSURL.fileURLWithPath_('/Applications'))
+        # set the default button name to
+        # 'Select' instead of the default 'Save'
+        panel.setPrompt_(u'Select')
+        # set a custom panel title
+        # instead of the default 'Open'
+        panel.setTitle_(u'Select an application')
+    
+        # show the Open panel
+        result = panel.runModal()
+        if result:
+            # return the pathname from the
+            # first (only) URL
+            return panel.URLs()[0].path()
+        else:
+            # user clicked Cancel
+            return None
+
+    
     def getSavePath_(self, app_path):
         '''Uses app_path to generated a suggested name for
         the package to be saved, then opens a Save panel. 
@@ -29,7 +60,7 @@ class MTMainWindowController(NSWindowController):
                       os.path.basename(app_path))[0]
         pkgname = appname + '.pkg'
         
-        # instantiate the save panel
+        # instantiate the save panel object
         panel = NSSavePanel.savePanel()
         # set default package name
         panel.setNameFieldStringValue_(pkgname)
@@ -54,6 +85,12 @@ class MTMainWindowController(NSWindowController):
             return None
 
     @IBAction
+    def getApplication_(self, sender):
+        pathname = self.getApplicationPath()
+        if pathname:
+            self.applicationFld.setStringValue_(pathname)
+            
+    @IBAction
     def buildPackage_(self, sender):
         applicationPath = self.applicationFld.stringValue()
         installLocation = self.installLocationFld.stringValue()
@@ -69,7 +106,7 @@ class MTMainWindowController(NSWindowController):
         NSLog('Save path: %s' % save_path)
 
     def awakeFromNib(self):
-        self.applicationFld.setStringValue_(u'/path/to/Some.app')
+        self.getApplication_(self)
         self.installLocationFld.setStringValue_(u'/Applications')
         self.identifierFld.setStringValue_(u'com.mactech.demo')
         self.versionFld.setStringValue_(u'1.0')
